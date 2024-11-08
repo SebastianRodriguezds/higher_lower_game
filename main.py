167d90cf40c4ccf2ba2f0c5fb2dd07aad545a901
Sebastian Rodriguez <sebastianrodriguezds@gmail.com>
2024-11-08 14:35:10 +0000

    Initial version; require refinament

--- main.py
+++ main.py
@@ -2,12 +2,45 @@ from logo import logo, vs
 from game_data import data
 import random
 
-# print(logo)
-
-current_score = 0
-while current_score < 10:
-  for opt in data:
-    print(random.choice(data))
+print(logo)
 
 
-user_answer = input("How has more followers? Type 'A' or 'B': ").lower()
+def game():
+  is_playing = True
+  current_score = 0
+  optionA = random.choice(data)
+  optionB = random.choice(data)
+  while current_score < 5 and is_playing:
+      
+      
+      print(f"Compare A: {optionA['name']}, a {optionA['description']}, from {optionA['country']}.")
+      print(vs)
+      print(f"Against B: {optionB['name']}, a {optionB['description']}, from {optionB['country']}.")
+
+      user_answer = input("How has more followers? Type 'A' or 'B': ").lower()
+
+
+      if optionA['follower_count'] > optionB['follower_count'] and user_answer == 'a':
+        current_score+=1
+        optionA = optionA
+        optionB = random.choice(data)
+        print(f"You're right! Current score: {current_score}.")
+
+      elif optionA['follower_count'] < optionB['follower_count'] and user_answer == 'b':
+        current_score+=1
+        optionA = optionB
+        optionB = random.choice(data)
+        print(f"You're right! Current score: {current_score}.")
+      else:
+        is_playing = False
+        print(f"Sorry, that's wrong. Final score: {current_score}")
+
+game()
+
+
+
+
+
+
+
+  
